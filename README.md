ad82198f7fa2bd8b3f00f6512450eccddf5d172e
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-05-21 22:57:20 -0500

    Add web content. (#1)
    * Develop in new branch:
    Port forwarding is down. Going to work on this elsewhere.
    
    * Test 1.
    
    * Test 2.
    
    * Test 3.
    
    * Test 4.
    
    * Test 5.
    
    * Test 6.
    
    * Test 7.
    
    * Test 8.
    
    * Test 9.
    
    * Test 10.
    
    * Test 11.
    
    * Add timestamps. What a trouble that port forwarding is broken today. Look at all the action runs I had to do!
    
    * Avoid unintentional jump.
    
    * Adjustments.
    
    * More adjustments.
    
    * Fix deprecation.
    
    * Update README.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # physics-work
-Explanation of the concept of work in physics using the Manim library.
+Explanation of the concept of work in physics using the Manim library by 3b1b.
 ## Manim
-Math animation library by 3b1b. This project uses the community version. Search GitHub.  
+This project uses the community version.  
 `Manim Community v0.17.3`
+## Jekyll
+Using Jekyll to mimic the GitHub Pages environment.
+> *Port forwarding was broken on 5/21/2023, so a bunch of action runs were used instead for testing...*
---
+++ main.js
@@ -0,0 +1,31 @@
+var video = document.getElementById("physics-work");
+
+const pauseTimes = [2, 8, 17, 21, 27, 31, 35, 37, 43, 46, 52, 57, 63, 69, 72, 77, 83, 86, 92, 103, 108, 114];
+
+function checkAutoPause() {
+    var currentSecond = Math.floor(video.currentTime);
+    if (pauseTimes.includes(currentSecond)) {
+        video.pause();
+        video.currentTime = currentSecond + 1;
+    }
+}
+
+video.addEventListener("timeupdate", checkAutoPause);
+
+video.addEventListener("mouseenter", function() {
+    video.controls = true;
+});
+video.addEventListener("mouseleave", function() {
+    video.controls = false;
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === ' ') {
+        event.preventDefault();
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    }
+});
