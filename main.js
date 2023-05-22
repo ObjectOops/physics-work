a593e96a84e6e0c55e1a32a4cd11e988cee760b5
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-05-21 20:40:58 -0500

    Test 7.

--- main.js
+++ main.js
@@ -3,9 +3,18 @@ var video = document.getElementById("physics-work");
 const pauseTimes = [10, 20, 30];
 
 function checkAutoPause() {
-    if (pauseTimes.includes(Math.floor(video.currentTime))) {
+    var currentSecond = Math.floor(video.currentTime);
+    if (pauseTimes.includes(currentSecond)) {
         video.pause();
+        video.currentTime = currentSecond + 1;
     }
 }
 
 video.addEventListener("timeupdate", checkAutoPause);
+
+video.addEventListener("mouseenter", function() {
+    video.controls = true;
+});
+video.addEventListener("mouseleave", function() {
+    video.controls = false;
+});
