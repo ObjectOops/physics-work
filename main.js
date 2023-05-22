67158ee1638640851cee09abff5d817acdb81a74
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-05-21 19:12:42 -0500

    Test 1.

---
+++ main.js
@@ -0,0 +1,16 @@
+const video = document.getElementById('physics-work');
+
+const pauseTimes = [10, 20, 30];
+
+function pauseVideoAtTimes() {
+  currentTime = Math.floor(video.currentTime);
+  if (pauseTimes.includes(currentTime)) {
+    video.pause();
+  }
+}
+
+function jumpToTime(time) {
+  video.currentTime = time;
+}
+
+video.addEventListener('timeupdate', pauseVideoAtTimes);
