3aae2d40162d44ec0c82d52e178eddc8258e574e
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-05-21 20:32:38 -0500

    Test 6.

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 var video = document.getElementById("physics-work");
 
-alert(video)
-
 const pauseTimes = [10, 20, 30];
 
 function checkAutoPause() {
-    document.getElementById("t").innerHTML = video.currentTime;
     if (pauseTimes.includes(Math.floor(video.currentTime))) {
         video.pause();
     }
 }
 
-video.addEventListener("loadedmetadata", function() {
-    video.addEventListener("timeupdate", checkAutoPause);
-});
+video.addEventListener("timeupdate", checkAutoPause);
